40987234809da0b0ced38ef8639217ea018c5a52
shreyeah38 <shreyarai0308@gmail.com>
2023-11-21 13:09:36 +0530

    hey

---
+++ app.js
@@ -0,0 +1,9 @@
+var btn = document.getElementById("btn");
+var light = document.getElementById("light");
+
+function toggleBtn()
+{
+    btn.classList.toggle("active");
+    light.classList.toggle("on");
+
+}
